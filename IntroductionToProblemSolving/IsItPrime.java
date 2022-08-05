f30bc89e3bcc5ac8ba5f50e3ab44d7d17237c204
Behera <mail2feeconbehera@gmail.com>
2022-08-05 21:32:18 +0530

    first commit

---
+++ IntroductionToProblemSolving/IsItPrime.java
@@ -0,0 +1,26 @@
+package IntroductionToProblemSolving;
+
+import java.util.Scanner;
+
+public class IsItPrime {
+  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int x = sc.nextInt();
+        isPrime(x);
+        
+    }
+    public static void isPrime(int A){
+        int count = 0;
+        for(int i = 2; i<=A ; i++){
+            if(A%i == 0){
+                count ++ ;
+            }
+        }
+        if(A==1) System.out.println("YES");
+        else if(count == 1) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
+
